8fca4d4bc454a04bc63c85e0f81cbaeb92831c41
tulXoro <tulxoro@hotmail.com>
2022-10-02 15:55:19 -0700

    Finished Tic Tac Toe

--- TikTacToe/Assignment-03_PA_TicTacToe.cpp
+++ TikTacToe/Assignment-03_PA_TicTacToe.cpp
@@ -46,13 +46,44 @@ int main() {
 }
 
 bool isWon(char player, char board[][3]) {
-	
-	return false;
+
+	if (board[0][0] == player && board[0][1] == player && board[0][2] == player) {
+		return true;
+	}
+	else if (board[1][0] == player && board[1][1] == player && board[1][2] == player) {
+		return true;
+	}
+	else if (board[2][0] == player && board[2][1] == player && board[2][2] == player) {
+		return true;
+	}
+	else if (board[0][0] == player && board[1][0] == player && board[2][0] == player) {
+		return true;
+	}
+	else if (board[0][1] == player && board[1][1] == player && board[2][1] == player) {
+		return true;
+	}
+	else if (board[0][2] == player && board[1][2] == player && board[2][2] == player) {
+		return true;
+	}
+	else if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+		return true;
+	}
+	else if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 bool isDraw(char board[][3]) {
 
-	return false;
+	if (board[0][0] != ' ' && board[0][1] != ' ' && board[0][2] != ' ' && board[1][0] != ' ' && board[1][1] != ' ' && board[1][2] != ' ' && board[2][0] != ' ' && board[2][1] != ' ' && board[2][2] != ' ') {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 void displayBoard(char board[][3]) {
