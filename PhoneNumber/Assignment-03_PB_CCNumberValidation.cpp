7f7d473558a4b8dcd196e52412d4a48b016f8d7c
Pokemontoy1234 <Brandon.brandonjohn32893289@yahoo.com>
2022-10-02 18:36:27 -0700

    Made tons of changes, check for errors?

--- PhoneNumber/Assignment-03_PB_CCNumberValidation.cpp
+++ PhoneNumber/Assignment-03_PB_CCNumberValidation.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 using namespace std;
 bool isvalidcc(const string&);
+int DoubleSum(const string&);
+int OddNumber(const string&);
+int getDigits(int numbers);
 
 int main()
 {
@@ -34,8 +37,69 @@ int main()
 }
 
 bool isvalidcc(const string& number) {
+
+	//start of length of credit card number
 	if (number.length() < 13 || number.length() > 16)
 		return false;
 	
+	//Start of what each credit card number is provided by
+		//if (number[0] == '4' || number[0] == '5' || number[0] == '4' ||
+		//	number[0] == '6' || number[0, 2] == '37') {
+
+	int sum1;
+	sum1 = DoubleSum(number);
+	int sum2;
+	sum2 = OddNumber(number);
+	int MainSum;
+	MainSum = sum1 + sum2;
+	if (MainSum % 10 == 0)
+		return true;
+
+	//end of addition of sums
+	else return false;
+	
+
+//End of in between 13 and 16
 	return true;
 }
+
+//Does the whole taking two digits and splitting them into 1
+int getDigits(int numbers) {
+	if (numbers > 9) {
+		int Numero = numbers % 10;
+		numbers = numbers / 10;
+		Numero += numbers;
+	}
+	else 
+return numbers;
+}
+
+//Creates the Double Sum
+int DoubleSum(string& number){
+	int sum = 0;
+	int SizeUp;
+	SizeUp = number.size();
+	for (int i = SizeUp - 2; i > 0; i -= 2) {
+		int Digits;
+		Digits = number[i] - '0';
+		sum = getDigits(Digits * 2) + sum;
+	}
+	return sum;
+}
+
+// Grabs the odd number sums
+int OddNumber(string& number) {
+	int sum = 0;
+	int SizeUp;
+	SizeUp = number.length();
+	for (int i = SizeUp - 1; i > 0; i -= 2) {
+		int Digits;
+		Digits = number[i] - '0';
+		sum = Digits + sum;
+	}
+	return sum;
+
+
+
+}
+
